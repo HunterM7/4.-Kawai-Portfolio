47f4cc3bea8ddc9fce982cd5cf229f372d9554f0
HunterM7 <tonym7@yandex.ru>
2022-03-31 15:53:58 +0300

    upd

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Scrolling
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+console.log(anchors)
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', event => {
+		event.preventDefault();
+
+		const blockID = anchor.getAttribute('href');
+
+		document.querySelector(`${blockID}`).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
+}
+
+
+// ------------------------------------
